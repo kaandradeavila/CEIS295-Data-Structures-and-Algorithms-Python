5e025571ea9d6125c9923514903e668f70c60c35
kaandradeavila <141256253+kaandradeavila@users.noreply.github.com>
2023-08-02 14:23:32 -0400

    Add files via upload

---
+++ Week 2 Project/Node.py
@@ -0,0 +1,7 @@
+#Name: Kyrenia Ailen Andrade
+#Date: 07/16/2022
+
+class Node:
+    def __init__(self, initial_data=None):
+        self.data = initial_data
+        self.next = None
---
+++ Week 3 Project/Node.py
@@ -0,0 +1,7 @@
+#Name: Kyrenia Ailen Andrade Avila
+#Date: 07/23/2022
+
+class Node:
+    def __init__(self, initial_data=None):
+        self.data = initial_data
+        self.next = None
